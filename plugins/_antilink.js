325480088090d1e0893fdfed4633f0d4e7265db0
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2022-06-01 11:26:12 +0300

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -6,7 +6,7 @@ module.exports = {
     let isGroupLink = linkRegex.exec(m.text)
 
     if (chat.antiLink && isGroupLink) {
-      m.reply('Hapus!!\n\nLink Grup terdeteksi')
+      m.reply('Hapus!!\n\nGroup link Detected')
       if (global.opts['restrict']) {
         if (isAdmin || !isBotAdmin) return true
         // this.groupRemove(m.chat, [m.sender])
