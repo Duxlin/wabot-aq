952bcc20340a92e1f38f5d7853e600a438eb4dd3
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2022-06-01 10:46:26 +0300

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -10,7 +10,7 @@ let handler = async (m, { args, usedPrefix, command }) => {
     repo = repo.replace(/.git$/, '')
     let url = `https://api.github.com/repos/${user}/${repo}/zipball`
     let filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
-    // 'attachment; filename=Nurutomo-wabot-aq-v2.5.1-251-g836cccd.zip'
+    // 'attachment; filename=Kanambp-kanambo-1-v2.5.1-251-g836cccd.zip'
     m.reply(`*Mohon tunggu, sedang mengirim repository..*`)
     conn.sendFile(m.chat, url, filename, null, m)
 
